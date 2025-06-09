f82dc56f7e3091c63778b999189364b8e6920c63
michalslowikowski <michalslowikowski00@gmail.com>
2025-06-09 11:00:44 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,49 +1,51 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require('eslint/config');
 
-const tsParser = require("@typescript-eslint/parser");
-const globals = require("globals");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
-const js = require("@eslint/js");
+const tsParser = require('@typescript-eslint/parser');
+const globals = require('globals');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
-        parserOptions: {},
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parserOptions: {},
 
-        globals: {
-            ...globals.node,
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
     },
 
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
 
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    extends: compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+    ),
     rules: {},
-}, {
-    files: ["**/*.spec.ts", "**/*.test.ts"],
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts'],
 
     languageOptions: {
-        globals: {
-            ...globals.jest,
-            ...globals.node,
-        },
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+      },
     },
-}, globalIgnores(["**/node_modules", "**/dist", "**/test-results"])]);
+  },
+  globalIgnores(['**/node_modules', '**/dist', '**/test-results']),
+]);
